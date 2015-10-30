0a8a7e2843e3330c7b5d6c1d5f6a6a869723246c
hayduke19us <hayduke19us@gmail.com>
2015-10-30 13:06:07 -0700

    add paper stack

--- client/components/actions/login.jsx
+++ client/components/actions/login.jsx
@@ -5,28 +5,41 @@ var Icons = require('../layout/icons.jsx')
 module.exports = React.createClass({
 
   render: function(){
-    var url = './wood.jpg'
-    var style = {
-      backgroundImage: 'url(' + url +')'
+    var woodUrl = './wood-back-lg.jpg'
+    var paperUrl = './paperball-stack.png'
+
+    var woodStyle = {
+      backgroundImage: 'url(' + woodUrl +')'
     }
+
+    var paperBalls = {
+      backgroundImage: 'url(' + paperUrl + ')',
+      height: '300px',
+    }
+
     return  (
-      <div className='container-fluid blue-back'>
-        <div className='row'>
-          <div className='col-lg-8 white'>
-            <h1>I forget to buy groceries. I even forget the 
-            <strong> Grocery List.</strong></h1>
-            <div className='center vcenter'>
-              <image src='./notepaper.png' alt='No Image'/>
+      <div className='vextend' style={woodStyle}>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-lg-8 white'>
+              <div className='center vcenter'>
+                <image src='./notepaper.png' alt='No Image'/>
+              </div>
+            </div>
+            <div className='col-lg-4'>
+              <div className='widget white vcenter'>
+                <h1>Login</h1>
+                <h2>Email</h2>
+                <input className='form-control' type='text' name='email' />
+                <h2>Password</h2>
+                <input className='form-control' type='text' name='email' />
+              </div>
             </div>
           </div>
-          <div className='col-lg-4'>
-            <div className='widget garden white'>
-              <h1>Login</h1>
-              <h2>Email</h2>
-              <input className='form-control' type='text' name='email' />
-              <h1>Password</h1>
-              <input className='form-control' type='text' name='email' />
-            </div>
+        </div>
+        <div id='footer'>
+          <div className='container-fluid'>
+            <div style={paperBalls}></div>
           </div>
         </div>
       </div>
